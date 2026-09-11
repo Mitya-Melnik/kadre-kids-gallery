ed9dccb22102cf3403b6900192e2f3d7538d06cc
Митя Мельник <mityamelnik@Noutbuk-Mita.local>
2026-09-11 16:31:11 +0300

    Use corrected school group portraits

--- src/components/school/SchoolStories.tsx
+++ src/components/school/SchoolStories.tsx
@@ -7,7 +7,7 @@ type SchoolStoryImage = {
   kind: "portrait" | "group" | "life";
 };
 
-const SCHOOL_STORY_ASSET_VERSION = "2026-09-11-3";
+const SCHOOL_STORY_ASSET_VERSION = "2026-09-11-4";
 
 const schoolStoryImages: SchoolStoryImage[] = [
   { slug: "portrait-girl-glasses", alt: "Портрет выпускницы в светлом образе", kind: "portrait" },
