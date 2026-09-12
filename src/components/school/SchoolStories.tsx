6c8282cebb7dc579d93dde25bb9c9e34b5eb2181
Митя Мельник <mityamelnik@Noutbuk-Mita.local>
2026-09-12 15:34:53 +0300

    Заменить фотографию класса в спортзале

--- src/components/school/SchoolStories.tsx
+++ src/components/school/SchoolStories.tsx
@@ -15,7 +15,7 @@ type SchoolStoryImage = {
   kind: "portrait" | "group" | "life";
 };
 
-const SCHOOL_STORY_ASSET_VERSION = "2026-09-11-4";
+const SCHOOL_STORY_ASSET_VERSION = "2026-09-12-1";
 
 const schoolStoryImages: SchoolStoryImage[] = [
   { slug: "portrait-girl-glasses", alt: "Портрет выпускницы в светлом образе", kind: "portrait" },
